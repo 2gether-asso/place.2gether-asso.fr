59778e9b95f95dad4d4a7a32aa54913e0ed430bc
Matiboux <matiboux@gmail.com>
2023-12-24 06:41:54 +0100

    Add description

--- app/app/src/i18n/locales/fr.ts
+++ app/app/src/i18n/locales/fr.ts
@@ -5,6 +5,10 @@ const locale =
 	'What is {0}?': 'Qu\'est-ce que {0} ?',
 	'Visit our website': 'Visite notre site web',
 	'Join our Discord': 'Rejoins notre Discord',
+	'A platform that allows you to place pixels throughout the year to make a final fresco like the r/place of reddit but in our own way.':
+		'Une plateforme vous permettant de poser tout au long de l\'année des pixels pour faire une fresque finale comme le r/place de reddit mais à notre sauce.',
+	'So, if you are in love with pixel art, see you in 2024!':
+		'Alors, si vous êtes amoureux du pixel art, rendez vous en 2024 !',
 } as const
 
 // Static type check
